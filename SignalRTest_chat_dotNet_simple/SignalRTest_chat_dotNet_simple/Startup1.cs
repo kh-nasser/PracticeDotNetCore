650ef49775d8635216d764b89bcbead2e7c5bb8f
nas.kh <nasser.fet@gmail.com>
2022-06-26 18:17:39 +0430

    added owin startup added app.MapSignalR()

---
+++ SignalRTest_chat_dotNet_simple/SignalRTest_chat_dotNet_simple/Startup1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SignalRTest_chat_dotNet_simple.Startup1))]
+
+namespace SignalRTest_chat_dotNet_simple
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
