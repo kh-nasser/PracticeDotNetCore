f077464441760279de20ffcd4860cdfceaea4fd4
nas.kh <nasser.fet@gmail.com>
2022-07-02 12:12:27 +0430

    register new user using userManager

--- token-based-authentication/token-based-authentication/Controllers/AuthenticationController.cs
+++ token-based-authentication/token-based-authentication/Controllers/AuthenticationController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using token_based_authentication.Data.Models;
+using token_based_authentication.Data.ViewModels;
 
 namespace token_based_authentication.Controllers
 {
@@ -20,6 +21,34 @@ namespace token_based_authentication.Controllers
             this.roleManager = roleManager;
             this.context = context;
             this.configuration = configuration;
+        }
+
+        [HttpPost("register-user")]
+        public async Task<IActionResult> Register([FromBody] RegisterVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Please, provide all the required fields");
+            }
+
+            var userExist = await userManager.FindByEmailAsync(model.EmailAddrress);
+            if (userExist != null)
+            {
+                return BadRequest($"User, {model.EmailAddrress} already exist");
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                UserName = model.UserName,
+                Email = model.EmailAddrress,
+                SecurityStamp = Guid.NewGuid().ToString(),
+            };
+
+            var result = await userManager.CreateAsync(newUser, model.Password);
+            if (result.Succeeded) return Ok("User created");
+            return BadRequest("User could not be created");
         }
     }
 }
---
+++ token-based-authentication/token-based-authentication/Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace token_based_authentication.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddrress{ get; set; }
+        [Required]
+        public string UserName{ get; set; }
+        [Required]
+        public string Password{ get; set; }
+    }
+}
